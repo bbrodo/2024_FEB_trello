726343ff4e612ff21f75146ef06b2225197d4f0c
bbrodo <15292@coderacademy.edu.au>
2024-07-05 16:53:12 +1000

    added authorisation checks

--- controllers/card_controller.py
+++ controllers/card_controller.py
@@ -4,6 +4,7 @@ from models.card import Card, card_schema, cards_schema
 from datetime import date
 from flask_jwt_extended import jwt_required, get_jwt_identity
 from controllers.comment_controller import comments_bp
+from utils import authorise_as_admin
 
 cards_bp = Blueprint("cards", __name__, url_prefix="/cards")
 cards_bp.register_blueprint(comments_bp)
@@ -43,6 +44,11 @@ def create_card():
 @cards_bp.route("/<int:card_id>", methods=["DELETE"])
 @jwt_required()
 def delete_card(card_id):
+
+    is_admin = authorise_as_admin()
+    if not is_admin or str(card.user_id) != get_jwt_identity():
+        return {"error": "User is not authorised to perform this action"}, 403
+
     stmt = db.select(Card).filter_by(id=card_id)
     card = db.session.scalar(stmt)
 
@@ -62,6 +68,8 @@ def update_card(card_id):
     card = db.session.scalar(stmt)
 
     if card:
+        if str(card.user_id) != get_jwt_identity():
+            return {"error": "you are not the owner of the card"}, 403
         card.title = body_data.get("title") or card.title
         card.description = body_data.get("description") or card.description
         card.status = body_data.get("status") or card.status
@@ -72,3 +80,6 @@ def update_card(card_id):
         return card_schema.dump(card)
     else:
         return {"error": f"card with id {card_id} not found"}, 404
+    
+
+
