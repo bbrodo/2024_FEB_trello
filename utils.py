ebdf5ff25fa64d9043637932568ff934fa1ea8a0
bbrodo <15292@coderacademy.edu.au>
2024-07-12 20:28:08 +1000

    implement admin decorator

--- utils.py
+++ utils.py
@@ -1,3 +1,4 @@
+import functools
 from flask_jwt_extended import get_jwt_identity
 
 from init import db
@@ -10,3 +11,15 @@ def authorise_as_admin():
     user = db.session.scalar(stmt)
 
     return user.is_admin
+def auth_as_admin_decorator(fn):
+    @functools.wraps(fn)
+    def wrapper(*args, **kwargs):
+        user_id = get_jwt_identity()
+        stmt = db.select(User).filter_by(id=user_id)
+        user = db.session.scalar(stmt)
+        if user.is_admin:
+            return fn(*args, **kwargs)
+        else:
+            return {"error": "Only admin can perform this action"}, 403
+    
+    return wrapper
